9e2e280278e62155b2389c7a2ab37d939b7552e8
yuliya <yu227591@gmail.com>
2026-07-03 14:02:56 +0300

    init

---
+++ src/main/java/ru/vtb/java/pro/javaprohibernate/JavaProHibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.vtb.java.pro.javaprohibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vtb/java/pro/javaprohibernate/JavaProHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.java.pro.javaprohibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
